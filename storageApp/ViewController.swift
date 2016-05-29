9cf6f1231117e3eb58fbce7a6c5d3a787a07bf9a
Brock Oberhansley <oberhansley@me.com>
2016-05-29 14:28:20 -0600

    Initial Commit

---
+++ storageApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  storageApp
+//
+//  Created by Brock Oberhansley on 5/29/16.
+//  Copyright © 2016 Brock Oberhansley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
